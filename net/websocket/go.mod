108ae01099918d82c56bd76c79acae79b187b947
felix <779358334@qq.com>
2020-07-16 10:52:05 +0800

    添加websocket

---
+++ net/websocket/go.mod
@@ -0,0 +1,8 @@
+module websocket
+
+go 1.13
+
+require (
+	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+)
---
+++ net/websocket/websocket_test.go
@@ -0,0 +1,11 @@
+package websocket
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWebSocket(t *testing.T) {
+	fmt.Println("***********WebSocket测试************")
+	MainWebSocket()
+}
